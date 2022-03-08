9673796f1c0a2106104cbaab1ecc92ca4045576c
Cory <cory.leclair@gmail.com>
2022-03-08 11:08:46 -0500

    first commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using SimpleAPI.Controllers;
+namespace SimpleAPI.Test;
+
+public class UnitTest1
+{
+
+    WeatherForecast weather = new WeatherForecast();
+    
+    
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
